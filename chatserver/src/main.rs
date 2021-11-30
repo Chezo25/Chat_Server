21c2f9816576afe1e3555b667e28515ae842dd80
IslandBoyDev25 <chezling.stoddard@gmail.com>
2021-11-29 23:17:46 -0400

    adding sockets
    -add socket list

--- chatserver/src/main.rs
+++ chatserver/src/main.rs
@@ -1,4 +1,13 @@
+use tokio::{io::AsyncReadExt, net::TcpListener};
+
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("localhost:8000").await.unwrap(); // await allows the program to wait for a response
+
+    let ( mut socket, _addr) = listener.accept().await.unwrap();
+
+    let mut buffer = [0u8; 1024];
+
+    socket.read(&mut buffer).await.unwrap();
 }
