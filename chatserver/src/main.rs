cc63ee07b54c0fda6b36b2152efc6ec23899a4d0
IslandBoyDev25 <chezling.stoddard@gmail.com>
2021-12-04 23:36:01 -0400

    bacnfwd

--- chatserver/src/main.rs
+++ chatserver/src/main.rs
@@ -1,6 +1,7 @@
 use tokio::{
     io::{AsyncBufReadExt, AsyncWriteExt, BufReader}, 
     net::TcpListener,
+    sync::broadcast,
 };
 
 
@@ -8,24 +9,31 @@ use tokio::{
 async fn main() {
     let listener = TcpListener::bind("localhost:8000").await.unwrap(); // await allows the program to wait for a response
 
-    let ( mut socket, _addr) = listener.accept().await.unwrap();
-
-    let (reader,  mut writer) = socket.split();
-
-    let mut reader = BufReader::new(reader);
-
-    let mut line: String = String::new();
+    let (tx, rx) = broadcast::channel::<String>(20);
 
     loop{
+        let ( mut socket, _addr) = listener.accept().await.unwrap();
+
+        tokio::spawn(async move {
+
+            let (reader,  mut writer) = socket.split();
+
+            let mut reader = BufReader::new(reader);
+
+            let mut line: String = String::new();
+
+            loop{
     
 
-    let bytes_read = reader.read_line(&mut line).await.unwrap();
-    if bytes_read == 0{
-        break;
-    }
+            let bytes_read = reader.read_line(&mut line).await.unwrap();
+            if bytes_read == 0{
+            break;
+            }
 
-        writer.write_all(line.as_bytes()).await.unwrap();
-        line.clear();
+            writer.write_all(line.as_bytes()).await.unwrap();
+            line.clear();
 
+            }
+        });
     }
 }
